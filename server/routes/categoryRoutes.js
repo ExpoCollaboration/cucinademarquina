a6577d262b05aa8a3d27ad603e35d1b2bdabe8a2
ExpoCollaboration <expocollaboration@gmail.com>
2024-05-31 01:24:07 +0800

    fix: import

--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -2,7 +2,7 @@ import {
     getAllCategory,
     saveCategory,
     getCategoryById
-} from '../controllers/CategoryController.js';
+} from '../controllers/categoryController.js';
 import express from 'express';
 
 const router = express.Router();
