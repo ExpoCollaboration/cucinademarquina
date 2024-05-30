075a338999e37233541a504907331b6fa872c8cc
ExpoCollaboration <expocollaboration@gmail.com>
2024-05-31 06:02:44 +0800

    fix: block by cors

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.use(passportSetup.session());
 // Middleware setup
 app.use(cors(
     {
-        origin: "*",
+        origin: ["http://localhost:5173", "https://versatily.website"],
         methods: "GET,POST,PUT,DELETE",
         credentials: true,
     }
