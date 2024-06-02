9e0dc58828e253a1312d356269a4f4afe8a69c23
ExpoCollaboration <expocollaboration@gmail.com>
2024-06-02 23:24:56 +0800

    fix: enable cors middleware

--- server/server.js
+++ server/server.js
@@ -41,13 +41,7 @@ app.use(passportSetup.initialize());
 app.use(passportSetup.session());
 
 // Middleware setup
-app.use(cors(
-    {
-        origin: ["http://localhost:5173", "https://versatily.website"],
-        methods: "GET,POST,PUT,DELETE",
-        credentials: true,
-    }
-));
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public")); // Serve static files from 'public' directory
 app.use(fileUpload({ createParentPath: true })); // File upload middleware
